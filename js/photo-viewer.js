3077e4025447606168ec6e40c8860a7f1c1c45ee
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2023-10-16 10:30:30 +0700

    edit

--- js/photo-viewer.js
+++ js/photo-viewer.js
@@ -1,29 +1,61 @@
-if (caches.hasOwProperty(scr)) {
-  if (cache[scr].isLoading === false) {
-    crossfade(cache[src].$img);
+var request;
+var $current;
+var cache = {};
+var $frame = $("#photo-viewer");
+var $thumbs = ".thumb";
+
+function crossfade($img) {
+  if ($current) {
+    $current.stop().fadeOut("slow");
   }
-} else {
-  $img = $("<img/>");
-  cache[src] = {
-    $img: $img,
-    isLoading: true,
-  };
 
-  $img.on("load", function () {
-    $img.hide();
-    $frame.removeClass("is-loading").append($img);
-    cache[src].isLoading = false;
-    if (request === src) {
-      crossfade($img);
-    }
+  $img.css({
+    marginLeft: -$img.width() / 2,
+    marginTop: -$img.height() / 2,
   });
 
-  $frame.addClass("is-loading");
+  $img.stop().fadeTo("slow", 1);
 
-  $img.attr({
-    src: src,
-    alt: this.title || "",
-  });
+  $current = $img;
 }
+
+$(document).on("click", ".thumb", function (e) {
+  var $img,
+    src = this.href;
+  request = src;
+
+  e.preventDefault();
+
+  $thumbs.removeClass("active");
+  $(this).addClass("active");
+
+  if (cache.hasOwnProperty(scr)) {
+    if (cache[scr].isLoading === false) {
+      crossfade(cache[src].$img);
+    }
+  } else {
+    $img = $("<img/>");
+    cache[src] = {
+      $img: $img,
+      isLoading: true,
+    };
+
+    $img.on("load", function () {
+      $img.hide();
+      $frame.removeClass("is-loading").append($img);
+      cache[src].isLoading = false;
+      if (request === src) {
+        crossfade($img);
+      }
+    });
+
+    $frame.addClass("is-loading");
+
+    $img.attr({
+      src: src,
+      alt: this.title || "",
+    });
+  }
+});
 
 $(".thumb").eq(0).click();
